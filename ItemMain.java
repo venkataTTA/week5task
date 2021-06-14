cce207ef793ef72cb24f27e564fc81475a0ff0b7
venkataTTA <84303270+venkataTTA@users.noreply.github.com>
2021-06-14 09:54:24 +0100

    Update ItemMain.java

--- ItemMain.java
+++ ItemMain.java
@@ -2,7 +2,7 @@ package venkataWeek5task;
 public class ItemMain {
     public static void main(String[] args) {
        // Item i1=new Item();
-       Item i2=new Item(01,"noodle",10.00,"chinese");
+       Item i2=new Item("01","noodle",10.00,"chinese");
        
        System.out.println("Itemid:"+i2.getItemid());
        System.out.println("Dish:"+i2.getDish());
